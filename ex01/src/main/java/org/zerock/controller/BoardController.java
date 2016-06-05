1e22e830acfd76bf95d4add73ebe6ce53322da60
ESKang <kes0228@gmail.com>
2016-06-05 11:10:52 +0900

    216 각목록 링크 처리 완료

--- ex01/src/main/java/org/zerock/controller/BoardController.java
+++ ex01/src/main/java/org/zerock/controller/BoardController.java
@@ -1,5 +1,7 @@
 package org.zerock.controller;
 
+import java.lang.ProcessBuilder.Redirect;
+
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
 
@@ -26,14 +29,20 @@ public class BoardController {
 	}
 	
 	@RequestMapping(value ="/register", method = RequestMethod.POST)
-	public String registPOST(BoardVO board, Model model) throws Exception {
+	public String registPOST(BoardVO board, RedirectAttributes rttr ) throws Exception {
 		logger.info("regist post ......");
 		logger.info(board.toString());
 		
 		service.regist(board);
-		model.addAttribute("result", "success");
+		rttr.addFlashAttribute("msg", "success");
 		
-		return "/board/success";	
+		return "redirect:/board/listAll";	
+	}
+	
+	@RequestMapping(value = "/listAll", method = RequestMethod.GET)
+	public void listAll(Model model) throws Exception {
+		logger.info("show all list................");
+		model.addAttribute("list", service.listAll());
 	}
 	
 	
