758f344182bfe5dd88f4a088648f06ed3ef832b7
ESKang <kes0228@gmail.com>
2016-06-11 14:32:49 +0900

    BoardTest 통과

--- ex01/src/main/java/org/zerock/persistence/BoardDAO.java
+++ ex01/src/main/java/org/zerock/persistence/BoardDAO.java
@@ -3,6 +3,7 @@ package org.zerock.persistence;
 import java.util.List;
 
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 public interface BoardDAO {
 	public void create(BoardVO vo) throws Exception;
@@ -15,5 +16,6 @@ public interface BoardDAO {
 	
 	public List<BoardVO> listAll() throws Exception;
 	
+	public List<BoardVO> listCriteria(Criteria cri) throws Exception;
 	
 }
--- ex01/src/test/java/org/zerock/test/BoardDAOTest.java
+++ ex01/src/test/java/org/zerock/test/BoardDAOTest.java
@@ -1,17 +1,24 @@
 package org.zerock.test;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 import org.zerock.persistence.BoardDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations ={"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
 public class BoardDAOTest {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BoardDAOTest.class);  
 	
 	@Inject
 	private BoardDAO dao;
@@ -33,6 +40,23 @@ public class BoardDAOTest {
 		board.setContent("수정 테스트"); 
 		dao.update(board);
 	}
+	
+	@Test
+	public void testListCriteria() throws Exception {
+		Criteria cri = new Criteria();
+		cri.setPage(2);
+		cri.setPerPageNum(20);
+		
+		List<BoardVO> list = dao.listCriteria(cri);
+		for  (BoardVO boardVO : list) {
+			logger.info(boardVO.getBno() + ":" + boardVO.getTitle());
+		}
+		
+
+	}
+	
+	
+	
 	
 	
 }
